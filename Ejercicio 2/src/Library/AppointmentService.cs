06ed7e7d55f802dad38413f765fcde7d2f682bdf
kevinalvarezvicente <64856845+kevinalvarezvicente@users.noreply.github.com>
2021-09-01 10:12:28 -0300

    Update AppointmentService.cs
    1.2

--- Ejercicio 2/src/Library/AppointmentService.cs
+++ Ejercicio 2/src/Library/AppointmentService.cs
@@ -14,41 +14,48 @@ namespace Library
         string doctorName;
         
         //No se desarolla el programa con una orientacion a objetos. En su lugar se utiliza un String Builder.
-        public static string CreateAppointment(string name, string id, string phoneNumber, DateTime date, string appoinmentPlace, string doctorName)
+        //public static string CreateAppointment(string name, string id, string phoneNumber, DateTime date, string appoinmentPlace, string doctorName)
+        public Appointment(string name, string id, string phoneNumber, DateTime date, string appoinmentPlace, string doctorName)
 
         {
-            StringBuilder stringBuilder = new StringBuilder("Scheduling appointment...\n");
-            Boolean isValid = true;
-
+         
+            //StringBuilder stringBuilder = new StringBuilder("Scheduling appointment...\n");
+            //Boolean isValid = true; No es necesaria esta variable ya que se utiliza return
+            /*
             if (string.IsNullOrEmpty(name)) // Cada uno de estos debe de ser un set que verifique si el atributo del objeto esta vacio.
             {
                 stringBuilder.Append("Unable to schedule appointment, Name is required\n");
-                isValid = false; //Deberia de retornar el valor false y detener la ejecucion del codigo para no consumir recursos.
+                //isValid = false; //Deberia de retornar el valor false y detener la ejecucion del codigo para no consumir recursos.
+                return false
             }
 
             if (string.IsNullOrEmpty(id))
             {
                 stringBuilder.Append("Unable to schedule appointment, id is required\n");
-                isValid = false;
+                //isValid = false;
+                return false
             }
 
             if (string.IsNullOrEmpty(phoneNumber))
             {
                 stringBuilder.Append("Unable to schedule appointment, Phone number is required\n");
-                isValid = false;
+                //isValid = false;
+                return false
             }
 
             if (string.IsNullOrEmpty(appoinmentPlace))
             {
                 stringBuilder.Append("Unable to schedule appointment, Appointment place is required\n");
-                isValid = false;
+                //isValid = false;
+                return false
             }
 
             
             if (string.IsNullOrEmpty(doctorName))
             {
                 stringBuilder.Append("Unable to schedule appointment, Doctor name is required\n");
-                isValid = false;
+                //isValid = false;
+                return false
             }
 
             if (isValid)
@@ -56,7 +63,8 @@ namespace Library
                 stringBuilder.Append("Appointment Scheduled");
             }
 
-            return stringBuilder.ToString();
+            return stringBuilder.ToString();*/
+            return true;
         }
 
     }
